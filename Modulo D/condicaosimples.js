efb99e9881cf34047a62039f4a5b3a89c8a7b5b2
willianproni <wifepro@hotmail.com>
2021-10-09 21:28:15 -0300

    exercicio realizado

---
+++ Modulo D/condicaosimples.js
@@ -0,0 +1,7 @@
+// var vel = 60
+// console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+// if (vel <= 70) {
+//   console.log(`Dirija sempre usando cinto de segurança`)
+// } else
+// console.log(`Você foi MULTADO`)
