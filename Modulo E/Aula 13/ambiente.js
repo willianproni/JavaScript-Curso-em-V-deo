91afcee5ae71de417118dbe221a9937ce8138385
willianproni <wifepro@hotmail.com>
2021-10-10 21:37:44 -0300

    for

--- Modulo E/Aula 13/ambiente.js
+++ Modulo E/Aula 13/ambiente.js
@@ -1,13 +1,18 @@
-var c = 1
-do {
-  console.log('passo', c)
-  c++
-} while (c <= 3)
+for (var c = 1; c <= 10; c++ ) {
+    console.log("Passo", c)
+}
 
-
+//while
 // var c = 1
 // while (c <= 100){
 //   console.log('passo', c)
 //   c++
 // }
+
+//do
+// var c = 1
+// do {
+//   console.log('passo', c)
+//   c++
+// } while (c <= 3)
 
