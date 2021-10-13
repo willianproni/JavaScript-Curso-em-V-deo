befbb018dfda8fcc3498022ffb8888974bd68fcc
willianproni <wifepro@hotmail.com>
2021-10-12 21:12:44 -0300

    ultimo ex

--- Modulo F/exercicio/exercicioanalise/script.js
+++ Modulo F/exercicio/exercicioanalise/script.js
@@ -3,13 +3,62 @@ let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
 
+  function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+      return true
+    } else {
+      return false
+    }
+  }
+
+  function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1) {
+      return true 
+    } else {
+      return false
+    }
+  }
+
 function adicionar() {
-  numero = Number(num.value) 
-  if(num.value.length == 0) {
-  alert('insira um valor')
-} else if (numero < 1 || numero > 100) {
-  alert('insira um numero entre 1 e 100')
-} else {
-  
+  let num = document.querySelector('input#fnum')
+  let lista = document.querySelector('select#flista')
+  let res = document.querySelector('div#res')
+  if(isNumero(num.value) && !inLista(num.value, valores)) {
+    valores.push(Number(num.value))
+    let item = document.createElement('option')
+    item.text = `Valor adicionado ${num.value}`
+    lista.appendChild(item)
+    res.innerHTML = ''
+  } else {
+    alert('Valor inválido ou já encontrado na lista')
+  }
+  num.value = ''
+  num.focus()
 }
+
+function finalizar() {
+  let res = document.querySelector('div#res')
+  if(valores.lenght == 0) {
+    alert('Adicione valores antes de finalizar')
+  } else {
+    let all = valores.length
+    let maior = valores[0]
+    let menor = valores[0]
+    let soma = 0
+    let media = 0
+    for (let cont in valores) {
+      soma += valores[cont]
+      if(valores[cont] > maior)
+      maior = valores[cont]
+      if(valores[cont] < menor)
+      menor = valores[cont]
+    }
+    media = soma / all
+    res.innerHTML = ''
+    res.innerHTML += `<p>Ao todo, temos ${all} números cadastrados</p>`
+    res.innerHTML += `<p>O maior valor é ${maior}`
+    res.innerHTML += `<p>O menor valor é ${menor}`
+    res.innerHTML += `<p>A soma de todos os valor é ${soma}`
+    res.innerHTML += `<p>A média entre os valores é ${media}`
+  }
 }
